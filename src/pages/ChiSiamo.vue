f5524b4b90b3e6c4cfb371cc9b5a508bff6a57c2
Michelazani <michi.zani220@gmail.com>
2024-02-27 17:12:59 +0100

    popolamento chi siamo

--- src/pages/ChiSiamo.vue
+++ src/pages/ChiSiamo.vue
@@ -1,7 +1,16 @@
 <template >
-    <h1>Chi siamo</h1>
-
-    
+    <div class="container">
+        <h2>Chi siamo</h2>
+        <p>Benvenuti nel mondo dei cocktail creati con passione e inventiva da quattro giovani talenti di Boolean Cocktail Academy! <br>
+            Siamo impegnati a offrire un servizio di qualità e a superare le aspettative dei nostri clienti. Oltre alla nostra dedizione alla perfezione nei cocktail, ci sforziamo di creare un'atmosfera accogliente e inclusiva, dove ogni ospite si senta parte di questa esperienza unica. <br>
+            Unisciti a noi nel nostro viaggio di scoperta dei sapori, delle tecniche e dell'arte dietro ogni cocktail. Siamo entusiasti di condividere la nostra passione con voi e di creare ricordi indimenticabili attraverso i nostri cocktail distintivi. Cheers! </p>
+        <h2>La Nostra Storia</h2>
+        <p>Ci siamo conosciuti durante il nostro percorso formativo presso la Boolean Cocktail Academy, dove abbiamo coltivato la nostra passione comune per l'arte della mixology. Ognuno di noi porta al tavolo competenze uniche e una prospettiva diversa, creando un team dinamico e creativo che non vede l'ora di condividere la nostra passione con voi.</p>
+        <h2>La Nostra Filosofia</h2>
+        <p>Presso Boolean Cocktail, crediamo che un buon cocktail non sia solo una bevanda, ma un'esperienza sensoriale che coinvolge tutti i sensi. Ci impegniamo a utilizzare ingredienti di alta qualità, combinati con tecniche innovative e una dose di creatività, per offrire cocktail che sorprendano e delizino ogni palato.</p>
+        <h2>I Nostri Cocktail</h2>
+        <p>Ogni cocktail che creiamo è il risultato di sperimentazioni, ispirazioni e la ricerca continua di perfezione. Dalle reinterpretazioni di classici intramontabili a creazioni originali che sfidano le aspettative, il nostro menu offre una gamma diversificata di opzioni per soddisfare ogni gusto.</p>
+    </div>
 </template>
 
 <script>
