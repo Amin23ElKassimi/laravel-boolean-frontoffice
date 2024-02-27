ee3d5e20b091bf3402a8572477237edf72161821
Michelazani <michi.zani220@gmail.com>
2024-02-27 17:01:01 +0100

    modifiche chi siamo

--- src/pages/ChiSiamo.vue
+++ src/pages/ChiSiamo.vue
@@ -1,43 +1,10 @@
-
-
 <template >
+    <h1>Chi siamo</h1>
 
+    
 </template>
 
 <script>
-import axios from 'axios';
-
-export default {
-    name: 'ProjectList',
-    data(){
-        return{
-          projects: [],
-        }
-    },
-    methods:{
-      getProject(){
-            axios.get('http://127.0.0.1:8000/api/posts', {
-                params: {
-                }
-            })
-            .then((response) => {
-                console.log(response.data.results.data);
-                this.projects = response.data.results.data;
-
-            })
-            .catch(function (error) {
-                console.warn(error);
-                this.$router.push({ name: 'not-found' })
-            })
-        }
-    },
-    components:{
-    },
-
-    created(){
-        this.getProject();
-    }
-}
 </script>
 
 
